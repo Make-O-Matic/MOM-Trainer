2bd4e8ada8f38548c74d262fc98fb1a6cc3afa83
ni-cc <ni-cc@users.noreply.github.com>
2017-03-25 18:55:12 +0100

    add videoeditor render settings

--- Slicer/videoeditor.py
+++ Slicer/videoeditor.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import argparse
-import datetime
 import gi
 gi.require_version('Gst', '1.0')
+gi.require_version('GstPbutils', '1.0')
 gi.require_version('GES', '1.0')
-from gi.repository import GLib, Gst, GES
+from gi.repository import GLib, Gst, GstPbutils, GES
 import arrow
 import helpers
 
@@ -13,7 +13,7 @@ def main():
     parser = argparse.ArgumentParser(description='videoeditor.')
     parser.add_argument('media', help='media file')
     parser.add_argument('-s', '--start',
-        help='start - "format 01.01.2000 01:02:03"',
+        help='start UTC - format "01.01.2000 01:02:03"',
         required=True, type=valid_time)
     helpers.add_db_arguments(parser)
     args = parser.parse_args()
@@ -21,12 +21,16 @@ def main():
     Gst.init(None)
     GES.init()
     loop = GLib.MainLoop()
+    discoverer = GstPbutils.Discoverer.new(600 * Gst.SECOND)
 
-    input = GES.UriClipAsset.request_sync('file://' + args.media)
+    media = 'file://' + args.media
+    encoding_profile = GstPbutils.EncodingProfile.from_discoverer(
+        discoverer.discover_uri(media))
+    input = GES.UriClipAsset.request_sync(media)
 
-    input_start = arrow.get(args.start)
+    input_start = args.start
     input_stop = input_start.shift(microseconds=
-        (input.get_duration()/Gst.MSECOND))
+        (input.get_duration()/Gst.USECOND))
 
     db = helpers.make_db(args)
     trainset_infos = helpers.get_trainset_infos(db)
@@ -36,37 +40,46 @@ def main():
         if clip_start < clip_end:
             timeline = GES.Timeline.new_audio_video()
             layer = timeline.append_layer()
-            clip = layer.add_asset(video, 0,
-                (clip_start - input_start).microsecond * Gst.MSECOND,
-                (clip_end - clip_start).microsecond * Gst.MSECOND,
+            clip = layer.add_asset(input, 0,
+                (clip_start - input_start).seconds * Gst.SECOND,
+                (clip_end - clip_start).seconds * Gst.SECOND,
                 GES.TrackType.UNKNOWN)
             timeline.commit()
             pipeline = GES.Pipeline()
             pipeline.set_timeline(timeline)
 
+            #pipeline.set_state(Gst.State.NULL)
+            if not pipeline.set_render_settings('file:///tmp/output.mp4',
+                encoding_profile):
+                raise argparse.ArgumentTypeError("Not a valid media file")
+            pipeline.set_mode(GES.PipelineFlags.RENDER)
+            pipeline.set_state(Gst.State.PLAYING)
+
             bus = pipeline.get_bus()
             bus.add_signal_watch()
-            bus.connect("message", handle_message)
+            bus.connect("message",
+                lambda bus, message: handle_message(bus, message, loop))
 
-            pipeline.set_render_settings('file:///home/gil/output.mp4')
-            pipeline.set_mode(GES.PipelineFlags.SMART_RENDER)
-            pipeline.set_state(Gst.State.PLAYING)
             loop.run()
+
 
 
 def valid_time(s):
     try:
-        return datetime.datetime.strptime(s, '%d.%m.%Y %H:%M:%S')
+        return arrow.get(s, 'DD.MM.YYYY HH:mm:ss')
     except ValueError:
         msg = "Not a valid date: '{0}'.".format(s)
         raise argparse.ArgumentTypeError(msg)
 
-def handle_message(bus, message):
+
+def handle_message(bus, message, loop):
     if message.type == Gst.MessageType.EOS:
         loop.quit()
     elif message.type == Gst.MessageType.ERROR:
         error = message.parse_error()
+        print error
         loop.quit()
+
 
 if __name__ == "__main__":
     main()

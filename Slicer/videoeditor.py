6cb85a2e4eea8cba9a782970f59abed158ab59cb
ni-cc <ni-cc@users.noreply.github.com>
2017-03-30 16:05:10 +0200

    add more info to videoeditor subtitles

--- Slicer/videoeditor.py
+++ Slicer/videoeditor.py
@@ -25,44 +25,59 @@ def main():
     loop = GLib.MainLoop()
     discoverer = GstPbutils.Discoverer.new(600 * Gst.SECOND)
 
-    media = 'file://' + args.media
+    media_uri = 'file://' + args.media
     encoding_profile = GstPbutils.EncodingProfile.from_discoverer(
-        discoverer.discover_uri(media))
-    input = GES.UriClipAsset.request_sync(media)
+        discoverer.discover_uri(media_uri))
+    input = GES.UriClipAsset.request_sync(media_uri)
 
     input_start = args.start
-    input_stop = input_start.shift(microseconds=
-        (input.get_duration()/Gst.USECOND))
+    input_stop = input_start.shift(seconds=(input.get_duration()/Gst.SECOND))
+    print(input_start,input_stop)
 
     db = helpers.make_db(args)
     trainset_infos = helpers.get_trainset_infos(db)
     for trainset in trainset_infos:
         info = trainset_infos[trainset]
-        clip_start = max(input_start, arrow.get(info['created']))
-        clip_stop = min(input_stop, arrow.get(info['ended']))
+        clip_start_us = helpers.get_endpoint(
+            db[trainset], { '_id' : { '$ne' : info['_id'] } }, True) / 1e6
+        clip_stop_us = helpers.get_endpoint(db[trainset], {}, False) / 1e6
+        trainset_stop = arrow.get(info['ended'])
+        trainset_start = trainset_stop.shift(
+            seconds=-(clip_stop_us - clip_start_us))
+        clip_start = max(input_start, trainset_start)
+        clip_stop = min(input_stop, trainset_stop)
         if clip_start < clip_stop:
-            clip_file = ('CLIP_' + trainset + '_' +
+            clip = ('CLIP_' + trainset + '_' +
                 info['experiment']['id'] + '_' +
                 info['parcours']['observer']['id'] + '_' +
                 info['parcours']['subject']['id'] + '_' +
                 info['parcours']['id'])
-            subtitle_file = clip_file + '.ass'
+            subtitle_file = clip + '.ass'
             subtitles = aeidon.Project()
 
-            clip_start_us = helpers.get_endpoint(
-                db[trainset], { '_id' : { '$ne' : info['_id'] } }, True)
+            offset = clip_start_us
 
             step = 1
             for exercise in helpers.get_exercises(db, info['parcours']['id']):
                 mutation = helpers.get_mutation(db, exercise)
                 filter = { 'step' :  step }
-                start = helpers.get_endpoint(db[trainset], filter, True)
-                stop = helpers.get_endpoint(db[trainset], filter, False)
+                start = helpers.get_endpoint(db[trainset], filter, True) / 1e6
+                stop = helpers.get_endpoint(db[trainset], filter, False) / 1e6
                 format = 'HH:mm:ss.SSS'
                 subtitle = aeidon.Subtitle(aeidon.modes.TIME)
-                subtitle.start = arrow.get((start - clip_start_us) / 1e6).format(format)
-                subtitle.end = arrow.get((stop - clip_start_us) / 1e6).format(format)
-                subtitle.main_text = mutation['id']
+                print((trainset_stop-trainset_start).seconds,
+                    (clip_stop-clip_start).seconds,
+                      clip_start_us, clip_stop_us, start, stop, offset)
+                subtitle.start = arrow.get(start - offset).format(format)
+                subtitle.end = arrow.get(stop - offset).format(format)
+                subtitle.main_text = str(step) + ' > ' + mutation['id']
+                if 'instruction' in mutation:
+                    subtitle.main_text += '\n' + mutation['instruction']
+                if 'hands' in mutation:
+                    host, spot, gesture, instruction = helpers.get_info(
+                        db, mutation['hands'], 'right')
+                    subtitle.main_text += '\n' + host + spot + gesture + instruction
+
                 subtitles.subtitles.append(subtitle)
                 step += 1
 
@@ -73,7 +88,7 @@ def main():
             layer = timeline.append_layer()
             layer.add_asset(input, 0,
                 (clip_start - input_start).seconds * Gst.SECOND,
-                (clip_stop - clip_start).seconds * Gst.SECOND,
+                ((clip_stop - clip_start).seconds + 1) * Gst.SECOND,
                 GES.TrackType.UNKNOWN)
             subtitle_overlay = GES.EffectClip.new('filesrc location=' +
                 subtitle_file + ' ! subtitleoverlay', '')
@@ -83,7 +98,7 @@ def main():
             pipeline = GES.Pipeline()
             pipeline.set_timeline(timeline)
             if not pipeline.set_render_settings(
-                'file://' + os.getcwd() + '/' + clip_file + '.mp4',
+                'file://' + os.getcwd() + '/' + clip + '.mp4',
                 encoding_profile):
                 raise argparse.ArgumentTypeError("Not a valid media file")
             pipeline.set_mode(GES.PipelineFlags.RENDER)
@@ -95,7 +110,6 @@ def main():
                 lambda bus, message: handle_message(bus, message, loop))
 
             loop.run()
-
 
 
 def valid_time(s):

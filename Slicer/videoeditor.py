b8fecaccae5821a835f36d6c60735f49ad9fb9f1
ni-cc <ni-cc@users.noreply.github.com>
2017-04-03 09:19:08 +0200

    modify videoeditor subtitling

--- Slicer/videoeditor.py
+++ Slicer/videoeditor.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
+import warnings
+warnings.filterwarnings('ignore')
 import os
 import argparse
 import re
@@ -16,11 +18,14 @@ def main():
     parser = argparse.ArgumentParser(description='videoeditor.')
     parser.add_argument('media', help='media file')
     parser.add_argument('-s', '--start',
-        help='start UTC - format "01.01.2000 01:02:03"',
+        help='Startzeit UTC - Format "01.01.2000 01:02:03"',
         required=True, type=valid_time)
     parser.add_argument('-a', '--align',
         choices=['left', 'right'], default='right',
         help='Alignment')
+    parser.add_argument('-m', '--nomute', action='store_const',
+        const=GES.TrackType.UNKNOWN, default=GES.TrackType.VIDEO,
+        help='Nicht stumm')
     helpers.add_db_arguments(parser)
     args = parser.parse_args()
     if args.align == 'right':
@@ -34,7 +39,9 @@ def main():
     discoverer = GstPbutils.Discoverer.new(600 * Gst.SECOND)
 
     media_uri = 'file://' + args.media
-    encoding_profile = GstPbutils.EncodingProfile.from_discoverer(
+    directory = args.media + arrow.now().format('-DDMMYYYYHHmmss')
+    os.mkdir(directory)
+    encoding_profile = GstPbutils.EncodingProfile..from_discoverer(
         discoverer.discover_uri(media_uri))
     input = GES.UriClipAsset.request_sync(media_uri)
 
@@ -55,17 +62,27 @@ def main():
         clip_start = max(input_start, trainset_start)
         clip_stop = min(input_stop, trainset_stop)
         if clip_start < clip_stop:
+            clip_duration = clip_stop - clip_start
+            offset = clip_start_us
+            #clip_stop_us - (trainset_stop - clip_start).seconds
+
             clip = ('CLIP_' + trainset + '_' +
                 info['experiment']['id'] + '_' +
                 info['parcours']['observer']['id'] + '_' +
                 info['parcours']['subject']['id'] + '_' +
                 info['parcours']['id'])
             subtitle_file = clip + '.ass'
+            subtitle = aeidon.Subtitle(aeidon.modes.TIME)
+            format = 'HH:mm:ss.SSS'
+            subtitle.start = arrow.get(0).format(format)
+            subtitle.end = arrow.get(clip_duration.seconds + 1).format(format)
+            subtitle.main_text = (trainset + '\n' +
+                info['experiment']['id'] + ' [' +
+                info['parcours']['subject']['id'] + ', ' +
+                info['parcours']['observer']['id'] + ']')
             subtitles = aeidon.Project()
+            subtitles.subtitles.append(subtitle)
 
-            offset = clip_start_us
-            #clip_stop_us - (trainset_stop - clip_start).seconds
-            print(trainset)
             step = 1
             for exercise in helpers.get_exercises(db, info['parcours']['id']):
                 mutation = helpers.get_mutation(db, exercise)
@@ -75,55 +92,60 @@ def main():
                 if not start:
                     step += 1
                     continue
-                format = 'HH:mm:ss.SSS'
                 subtitle = aeidon.Subtitle(aeidon.modes.TIME)
                 subtitle.start = arrow.get((start / 1e6) - offset).format(format)
                 subtitle.end = arrow.get((stop / 1e6) - offset).format(format)
-                subtitle.main_text = (trainset + '\n' +
-                    info['experiment']['id'] + ' [' +
-                    info['parcours']['subject']['id'] + ', ' +
-                    info['parcours']['observer']['id'] + ']\n' +
+                subtitle.main_text += ('\n' +
                     info['parcours']['id'] + '/' + str(step) + ' > ' +
-                    mutation['id'] + '\nR:' +
-                    info['parcours']['subject']['hands']['right']['id'])
+                    mutation['id'])
                 if 'hands' in mutation:
-                    host, spot, gesture, instruction = helpers.get_info(
-                        db, mutation['hands'], 'right')
-                    subtitle.main_text += ('[' +
-                        host + '>' + spot + ', ' + gesture + ']')
-                subtitle.main_text += ('\nL:' +
-                    info['parcours']['subject']['hands']['left']['id'])
-                if 'hands' in mutation:
-                    host, spot, gesture, instruction = helpers.get_info(
-                        db, mutation['hands'], 'left')
-                    subtitle.main_text += ('[' +
-                        host + '>' + spot + ', ' + gesture + ']')
+                    text, instruction = helpers.get_info(
+                        db, mutation['hands'], 'left', ', ')
+                    if text != None:
+                        subtitle.main_text += ('\nL:' +
+                            info['parcours']['subject']['hands']['left']['id'])
+                    if text:
+                        subtitle.main_text += ('[' + text + ']')
+                    text, instruction = helpers.get_info(
+                        db, mutation['hands'], 'right', ', ')
+                    if text != None:
+                        subtitle.main_text += ('\nR:' +
+                            info['parcours']['subject']['hands']['right']['id'])
+                    if text:
+                        subtitle.main_text += ('[' + text + ']')
 
                 subtitles.subtitles.append(subtitle)
                 step += 1
 
             subtitles_ass = aeidon.files.new(aeidon.formats.ASS,
-                subtitle_file, 'utf_8')
+                directory + '/' + subtitle_file, 'utf_8')
             subtitles_ass.header = re.sub(
-                '2, 30, 30, 30, 0$', args.align + ', 0, 0, 0, 0',
+                '2, 30, 30, 30, 0$', args.align + ', 30, 30, 30, 0',
                 subtitles_ass.header)
             subtitles.save_main(subtitles_ass)
 
             timeline = GES.Timeline.new_audio_video()
-            layer = timeline.append_layer()
-            layer.add_asset(input, 0,
+
+            #GES.Track.new(GES_TRACK_TYPE_TEXT, caps)
+            #timeline.add_track()
+            #"select-tracks-for-object"
+            subtitle_layer = timeline.append_layer()
+            subtitle_overlay = GES.EffectClip.new(
+                'filesrc location=' +
+                subtitle_file +
+                ' ! subtitleoverlay', '')
+            subtitle_layer.add_clip(subtitle_overlay)
+            input_layer = timeline.append_layer()
+            input_layer.add_asset(input, 0,
                 (clip_start - input_start).seconds * Gst.SECOND,
-                ((clip_stop - clip_start).seconds + 1) * Gst.SECOND,
-                GES.TrackType.UNKNOWN)
-            subtitle_overlay = GES.EffectClip.new('filesrc location=' +
-                subtitle_file + ' ! subtitleoverlay', '')
-            layer.add_clip(subtitle_overlay)
+                (clip_duration.seconds + 1) * Gst.SECOND,
+                args.nomute)
             timeline.commit()
 
             pipeline = GES.Pipeline()
             pipeline.set_timeline(timeline)
             if not pipeline.set_render_settings(
-                'file://' + os.getcwd() + '/' + clip + '.mp4',
+                'file://' + directory + '/' + clip + '.mp4',
                 encoding_profile):
                 raise argparse.ArgumentTypeError("Not a valid media file")
             pipeline.set_mode(GES.PipelineFlags.RENDER)

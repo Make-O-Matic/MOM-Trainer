74d833ad018d1b0a96d52833ced09c11b6bed5bd
ni-cc <ni-cc@users.noreply.github.com>
2017-06-05 16:57:00 +0200

    recorder.py: Add signal handling

--- Recorder/recorder.py
+++ Recorder/recorder.py
@@ -5,8 +5,12 @@ import datetime
 import threading
 from time import sleep
 import uuid
-import os, sys, tty, termios
+import os
+import sys
+import tty
+import termios
 from subprocess import call
+import signal
 
 from pymongo import MongoClient
 from momconnectivity.glove import Glove
@@ -14,13 +18,20 @@ from momconnectivity.glove import Glove
 
 def main():
     parser = argparse.ArgumentParser(description='recorder.')
-    parser.add_argument('-l', '--lId', required=True, help='COLLECTOR.id fuer den linken Handschuh')
-    parser.add_argument('-L', '--lMAC', required=True, help='COLLECTOR.macAddress fuer den linken Handschuh')
-    parser.add_argument('-r', '--rId', required=True, help='COLLECTOR.id fuer den rechten Handschuh')
-    parser.add_argument('-R', '--rMAC', required=True, help='COLLECTOR.macAddress fuer den rechten Handschuh')
-    parser.add_argument('-e', '--experiment', required=True, help='EXPERIMENT.id (neu)')
-    parser.add_argument('-o', '--observer', required=True, help='OBSERVER.id (neu)')
-    parser.add_argument('-s', '--subject', required=True, help='SUBJECT.id (neu)')
+    parser.add_argument('-l', '--lId', required=True,
+                        help='COLLECTOR.id fuer den linken Handschuh')
+    parser.add_argument('-L', '--lMAC', required=True,
+                        help='COLLECTOR.macAddress fuer den linken Handschuh')
+    parser.add_argument('-r', '--rId', required=True,
+                        help='COLLECTOR.id fuer den rechten Handschuh')
+    parser.add_argument('-R', '--rMAC', required=True,
+                        help='COLLECTOR.macAddress fuer den rechten Handschuh')
+    parser.add_argument('-e', '--experiment', required=True,
+                        help='EXPERIMENT.id (neu)')
+    parser.add_argument('-o', '--observer', required=True,
+                        help='OBSERVER.id (neu)')
+    parser.add_argument('-s', '--subject', required=True,
+                        help='SUBJECT.id (neu)')
 
     args = parser.parse_args()
 
@@ -41,61 +52,81 @@ def main():
 
     lUUID = str(uuid.uuid4())
     rUUID = str(uuid.uuid4())
+    
+    def end():
+        gloves[0].disconnect()
+        sys.exit()
+    
+    signal.signal(signal.SIGINT, end)
     try:
-        gloves = [Glove(args.lMAC, args.rMAC, set_connected, is_recording, lUUID, rUUID)]
+        gloves = [Glove(args.lMAC, args.rMAC, set_connected, is_recording,
+                        lUUID, rUUID)]
         gloves[0].state = 0
         gloves[0].recording = False
         gloves[0].both_connected = threading.Event()
         gloves[0].connect()
         gloves[0].both_connected.wait()
     except RuntimeError:
-            print('Verbindung zu (mind.) einem COLLECTOR konnte nicht hergestellt werden.')
-            print('Bitte pruefen Sie die Bluetooth-Verbindung und starten Sie das Programm erneut.')
-            print('Programm mit \'STRG+C\' beenden.')
-            while True:
-                try:
-                    sleep(1)
-                except KeyboardInterrupt:
-                    gloves[0].disconnect()
-                    sys.exit()
-
-
+        print('Verbindung zu (mind.) einem COLLECTOR konnte nicht hergestellt werden.')
+        print('Bitte pruefen Sie die Bluetooth-Verbindung und '
+              'starten Sie das Programm erneut.')
+        print('Programm mit \'STRG+C\' beenden.')
+        signal.pause()
 
     while True:
         args.parcours = input('Valide PARCOURS.id angeben um Aufzeichnung zu starten: ')
-        
+
         while True:
             parcours = db.parcours.find_one({'id': args.parcours})
             if bool(parcours):
                 break
-            args.parcours = input('PARCOURS.id "' + args.parcours + '" existiert nicht. Zum Aufzeichnen bitte valide PARKOUR.id angeben: ')
+            args.parcours = input(
+                'PARCOURS.id "' + args.parcours
+                + '" existiert nicht. Zum Aufzeichnen bitte valide PARKOUR.id angeben: ')
 
         now = datetime.datetime.utcnow()
         trainsetName = 'TRAINSET' + now.strftime('%d%m%Y%H%M%S')
-        print('PARCOURS gefunden. EXERCISEs werden geladen, ' + trainsetName + ' wird erstellt...\n')
+        print('PARCOURS gefunden. EXERCISEs werden geladen, '
+              + trainsetName + ' wird erstellt...\n')
         gloves[0].setTrainsetExercise(trainsetName, 0, '', '')
         trainset = db[trainsetName]
-        trainset.insert_one(
-        {   "_id" : trainsetName,
-            "created" : now,
-            "experiment" : { "id" : args.experiment },
-            "parcours" : { "id" : args.parcours,
-                          "observer" : { "id" : args.observer },
-                          "subject" : { "id" : args.subject,
-                                        "hands" : { "left" : { "uuid" : lUUID,
-                                                               "id" : args.lId,
-                                                               "macAddress" : args.lMAC },
-                                                    "right" : { "uuid" : rUUID,
-                                                               "id" : args.rId,
-                                                                "macAddress" : args.rMAC } } } } });
+        trainset.insert_one({
+            '_id': trainsetName,
+            'created': now,
+            'experiment': {'id': args.experiment},
+            'parcours': {
+                'id': args.parcours,
+                'observer': {'id': args.observer},
+                'subject': {
+                    'id': args.subject,
+                    'hands': {
+                        'left': {
+                            'uuid': lUUID,
+                            'id': args.lId,
+                            'macAddress': args.lMAC
+                        },
+                        'right': {
+                            'uuid': rUUID,
+                            'id': args.rId,
+                            'macAddress': args.rMAC
+                        }
+                    }
+                }
+            }
+        })
+
+        mutations = {}
+        for exercise in parcours['exercises']:
+            mutations[exercise['mutation']['id']] = db.mutations.find_one(
+                {'id': exercise['mutation']['id']})
 
         step = 1
-        cmd = ''
-        for exercise in parcours["exercises"]:
-            mutation = db.mutations.find_one({'id' : exercise['mutation']['id']})
-            if (bool(mutation) and 'slug' in mutation):
+        for exercise in parcours['exercises']:
+            mutation = mutations[exercise['mutation']['id']]
+            if 'slug' in mutation:
                 slug = mutation['slug']
-            print(str(step) + '/' + str(len(parcours['exercises'])) + ' (' + exercise['mutation']['id'] + ') "' + slug + '"')
+            print(str(step) + '/' + str(len(parcours['exercises']))
+                  + ' (' + exercise['mutation']['id'] + ') "' + slug + '"')
             step += 1
 
         print("----------------------")
@@ -106,34 +137,31 @@ def main():
         print('----------------------')
         print('Zum Starten des PARCOURS \'Leertaste\' druecken...')
         
+        cmd = ''
         while True:
             cmd = getch()
             if cmd == ' ':
                 break;
                 
         print('----------------------')
-        
-        mutationIndex = {}
-        for exercise in parcours['exercises']:
-            mutationIndex[exercise['mutation']['id']] = str(
-                db.mutations.find_one({'id' : exercise['mutation']['id']})['_id'])
 
         step = 1
         for exercise in parcours['exercises']:
-			#mutation = db.mutations.find_one({'id' : exercise['mutation']['id']})str(mutation['_id'])
+            mutation = mutations[exercise['mutation']['id']]
             gloves[0].setTrainsetExercise('', step, exercise['mutation']['id'], 
-                mutationIndex[exercise['mutation']['id']])
+                mutation['_id'])
             gloves[0].recording = True
+            if ('slug' in mutation):
+                slug = mutation['slug']
             print('Jetzt EXERCISE ' + str(step) + '/' + str(len(parcours['exercises'])) + 
-                  ' (' + exercise['mutation']['id'] + ') ausfuehren')
+                  ' (' + exercise['mutation']['id'] + ') "' + slug + '" ausfuehren')
 
-            if bool(mutation):
-                if 'instruction' in mutation:
-                    print('- INSTRUCTION: "' + mutation['instruction'] + '"')
-                if 'hands' in mutation:
-                    hands = mutation['hands']
-                    show_info(db, hands, 'left', 'linke')
-                    show_info(db, hands, 'right', 'rechte')
+            if 'instruction' in mutation:
+                print('- INSTRUCTION: "' + mutation['instruction'] + '"')
+            if 'hands' in mutation:
+                hands = mutation['hands']
+                print_info(db, hands, 'left', 'linke')
+                print_info(db, hands, 'right', 'rechte')
 
             print('----------------------')
             if exercise['signal']['beep']:
@@ -171,28 +199,17 @@ def main():
             cmd = getch()
             if cmd == ' ':
                 break;
-            if ord(cmd) == 3:
-               gloves[0].disconnect()
-               sys.exit()
-            sleep(1);
 
 
 def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+    return sys.stdin.read(1)
 
 
 def beep():
     call(['ogg123', '-q', '/usr/share/sounds/ubuntu/stereo/dialog-information.ogg'])
 
 
-def show_info(db, hands, side, description):
+def print_info(db, hands, side, description):
     text = ''
     if side in hands:
         hand = hands[side]
